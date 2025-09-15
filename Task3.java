f6e5ff4bdc23296356a4efb66c92ae3be9c62f5a
Polina Samigullina <p.samigullina@gmail.com>
2025-09-15 23:18:03 +0300

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент " + i + ": " + args[i]);
+        }
+    }
+}
---
+++ Task4.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("H(%d) = %.4f%n", i, 1.0 / i);
+        }
+    }
+}

588c118c3127b1b7faed1a05dc95892a3948c8a8
Polina Samigullina <p.samigullina@gmail.com>
2025-09-20 23:41:58 +0300

    Delete Task3.java

--- Task3.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab1;
-
-public class Task3 {
-    public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Аргумент " + i + ": " + args[i]);
-        }
-    }
-}

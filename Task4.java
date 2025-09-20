dd2d1c5970429d50fa0b4bf74bbe43083fb1e2cf
Polina Samigullina <p.samigullina@gmail.com>
2025-09-20 23:41:34 +0300

    Delete Task4.java

--- Task4.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab1;
-
-public class Task4 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.printf("H(%d) = %.4f%n", i, 1.0 / i);
-        }
-    }
-}
